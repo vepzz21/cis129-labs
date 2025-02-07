831da006a092d460712134924a145cc2e332b601
vepzz21 <vpinazelinger@mail.pima.edu>
2025-02-06 19:23:04 -0700

    Update cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,10 @@
-print('Hello, World!')
+# Author: Emmanuel Pina-Zelinger
+# Program Description: Prints a simple short poem.
+# Changes: Hello World statement deleted and replaced with simple poem. 
+story = 'Golf is a beautiful game I\'ve come to learn.\n\
+Golf is a beautiful game one may discern.\n\
+Golf is not for the faint of heart.\n\
+Golf is just a game for those who start.\n\
+One quickly comes to find after playing awhile\n\
+Golf is more than a game; it\'s a lifestyle.'
+print(story)
