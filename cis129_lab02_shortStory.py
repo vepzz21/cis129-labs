94743506411fb508613cc231fab983a36a031eab
vepzz21 <vpinazelinger@mail.pima.edu>
2025-02-06 19:37:44 -0700

    Update cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -5,6 +5,6 @@ story = 'Golf is a beautiful game I\'ve come to learn.\n\
 Golf is a beautiful game one may discern.\n\
 Golf is not for the faint of heart.\n\
 Golf is just a game for those who start.\n\
-One quickly comes to find after playing awhile\n\
+One quickly comes to find after playing awhile.\n\
 Golf is more than a game; it\'s a lifestyle.'
 print(story)
